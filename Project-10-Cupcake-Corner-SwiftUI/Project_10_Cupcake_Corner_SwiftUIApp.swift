1a6656cd5497a320e3b348a9fc6683032a5abf23
Baris <karalarbaris@gmail.com>
2021-06-27 10:11:17 +0200

    Initial Commit

---
+++ Project-10-Cupcake-Corner-SwiftUI/Project_10_Cupcake_Corner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_10_Cupcake_Corner_SwiftUIApp.swift
+//  Project-10-Cupcake-Corner-SwiftUI
+//
+//  Created by Baris Karalar on 27.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_10_Cupcake_Corner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
