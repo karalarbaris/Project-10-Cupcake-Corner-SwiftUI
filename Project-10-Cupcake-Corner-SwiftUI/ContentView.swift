95a1f1ea9c274c2b5d513e928467743700407e7d
Baris <karalarbaris@gmail.com>
2021-06-28 11:37:52 +0200

    Sending and receiving Codable data with URLSession and SwiftUI

--- Project-10-Cupcake-Corner-SwiftUI/ContentView.swift
+++ Project-10-Cupcake-Corner-SwiftUI/ContentView.swift
@@ -7,34 +7,107 @@
 
 import SwiftUI
 
-//Adding Codable conformance for @Published properties
+//Sending and receiving Codable data with URLSession and SwiftUI
 
-class User: ObservableObject, Codable {
-    
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "Baris Karalar"
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-    
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct Response: Codable {
+    var results: [Result]
 }
 
 struct ContentView: View {
+    
+    @State private var results = [Result]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        List(results, id: \.trackId) { item in
+            VStack {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+                
+            }
+        }
+//        .onAppear(perform: loadData)
+        .onAppear(perform: {
+            loadData()
+        })
+        
+        
+    }
+    
+    func loadData() {
+        
+//        1. Creating the URL we want to read.
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid url")
+            return
+        }
+        
+//        2. Wrapping that in a URLRequest, which allows us to configure how the URL should be accessed.
+        let request = URLRequest(url: url)
+        
+//        3. Create and start a networking task from that URL request.
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            //        4. Handle the result of that networking task.
+            if let data = data {
+                
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    // we have good data – go back to the main thread
+                    DispatchQueue.main.async {
+                        //update the ui
+                        self.results = decodedResponse.results
+                        
+                        // everything is good, so we can exit
+                        return
+                    }
+                }
+                // if we're still here it means there was a problem
+                print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+                
+                
+            }
+            
+            
+        }.resume()
+        
+        
+        
     }
 }
+
+
+
+////Adding Codable conformance for @Published properties
+//
+//class User: ObservableObject, Codable {
+//
+//    enum CodingKeys: CodingKey {
+//        case name
+//
+//    }
+//
+//    @Published var name = "Baris Karalar"
+////    var age = "34"
+////    @Published var nickName = "karalarbaris"
+//
+//    required init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        name = try container.decode(String.self, forKey: .name)
+//    }
+//
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(name, forKey: .name)
+//    }
+//
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
