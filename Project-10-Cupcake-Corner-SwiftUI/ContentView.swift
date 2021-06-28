5e76f7225ba05bec52577df66d78160fc57d868a
Baris <karalarbaris@gmail.com>
2021-06-28 11:53:01 +0200

    Validating and disabling forms

--- Project-10-Cupcake-Corner-SwiftUI/ContentView.swift
+++ Project-10-Cupcake-Corner-SwiftUI/ContentView.swift
@@ -7,80 +7,113 @@
 
 import SwiftUI
 
-//Sending and receiving Codable data with URLSession and SwiftUI
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
-struct Response: Codable {
-    var results: [Result]
-}
+//Validating and disabling forms
 
 struct ContentView: View {
     
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
     
     var body: some View {
         
-        List(results, id: \.trackId) { item in
-            VStack {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-                
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-        }
-//        .onAppear(perform: loadData)
-        .onAppear(perform: {
-            loadData()
-        })
-        
-        
-    }
-    
-    func loadData() {
-        
-//        1. Creating the URL we want to read.
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid url")
-            return
-        }
-        
-//        2. Wrapping that in a URLRequest, which allows us to configure how the URL should be accessed.
-        let request = URLRequest(url: url)
-        
-//        3. Create and start a networking task from that URL request.
-        URLSession.shared.dataTask(with: request) { data, response, error in
             
-            //        4. Handle the result of that networking task.
-            if let data = data {
-                
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data – go back to the main thread
-                    DispatchQueue.main.async {
-                        //update the ui
-                        self.results = decodedResponse.results
-                        
-                        // everything is good, so we can exit
-                        return
-                    }
+            Section {
+                Button("Create account") {
+                    print("Creating account")
                 }
-                // if we're still here it means there was a problem
-                print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-                
-                
             }
+            //            .disabled(username.isEmpty || email.isEmpty)
+            .disabled(disableForm)
             
             
-        }.resume()
-        
-        
+        }
         
     }
 }
+
+////Sending and receiving Codable data with URLSession and SwiftUI
+//
+//struct Result: Codable {
+//    var trackId: Int
+//    var trackName: String
+//    var collectionName: String
+//}
+//
+//struct Response: Codable {
+//    var results: [Result]
+//}
+//
+//struct ContentView: View {
+//
+//    @State private var results = [Result]()
+//
+//    var body: some View {
+//
+//        List(results, id: \.trackId) { item in
+//            VStack {
+//                Text(item.trackName)
+//                    .font(.headline)
+//                Text(item.collectionName)
+//
+//            }
+//        }
+////        .onAppear(perform: loadData)
+//        .onAppear(perform: {
+//            loadData()
+//        })
+//
+//
+//    }
+//
+//    func loadData() {
+//
+////        1. Creating the URL we want to read.
+//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+//            print("Invalid url")
+//            return
+//        }
+//
+////        2. Wrapping that in a URLRequest, which allows us to configure how the URL should be accessed.
+//        let request = URLRequest(url: url)
+//
+////        3. Create and start a networking task from that URL request.
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//
+//            //        4. Handle the result of that networking task.
+//            if let data = data {
+//
+//                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+//                    // we have good data – go back to the main thread
+//                    DispatchQueue.main.async {
+//                        //update the ui
+//                        self.results = decodedResponse.results
+//
+//                        // everything is good, so we can exit
+//                        return
+//                    }
+//                }
+//                // if we're still here it means there was a problem
+//                print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+//
+//
+//            }
+//
+//
+//        }.resume()
+//
+//
+//
+//    }
+//}
 
 
 
