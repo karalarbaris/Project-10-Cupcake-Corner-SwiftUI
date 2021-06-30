3b4205c674c70108a564c51d2ea7ac91d15990b5
Baris <karalarbaris@gmail.com>
2021-06-30 19:41:06 +0200

    Sending and receiving orders over the internet

--- Project-10-Cupcake-Corner-SwiftUI/CheckoutView.swift
+++ Project-10-Cupcake-Corner-SwiftUI/CheckoutView.swift
@@ -11,6 +11,10 @@ struct CheckoutView: View {
     
     @ObservedObject var order: Order
     
+    @State private var conformationMessage = ""
+    @State private var showingConfirmation = false
+    
+    
     var body: some View {
         
         GeometryReader { geo in
@@ -26,16 +30,56 @@ struct CheckoutView: View {
                         .font(.title)
                     
                     Button("Place Order") {
-                        
+                        placeOrder()
                     }
                     .padding()
                 }
             }
         }
         .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation, content: {
+            Alert(title: Text("a asdf sfsd"), message: Text(conformationMessage), dismissButton: .default(Text("Okk")))
+        })
         
         
     }
+    
+    func placeOrder() {
+        //  1. Convert our current order object into some JSON data that can be sent.
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("failed to encode order")
+            return
+        }
+        
+        //  2. Prepare a URLRequest to send our encoded data as JSON.
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        //  3. Run that request and process the response.
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            guard let data = data else {
+                print("no data in response: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                conformationMessage = "Your order for \(decodedOrder.quantity) x \(Order.types[decodedOrder.type].lowercased()) is on its way, hurray!!!"
+                showingConfirmation = true
+            } else {
+                print("Invalid response from server")
+            }
+            
+            
+        }.resume()
+        
+        
+    }
+    
+    
 }
 
 struct CheckoutView_Previews: PreviewProvider {
