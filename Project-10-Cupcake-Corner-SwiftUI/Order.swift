87a8c35f4778e40bb15f1815e7e924ca977a18e5
Baris <karalarbaris@gmail.com>
2021-06-30 18:52:44 +0200

    Encoding an ObservableObject class

--- Project-10-Cupcake-Corner-SwiftUI/Order.swift
+++ Project-10-Cupcake-Corner-SwiftUI/Order.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-class Order: ObservableObject {
+//Encoding an ObservableObject class
+
+class Order: ObservableObject, Codable {
     
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
@@ -25,10 +27,10 @@ class Order: ObservableObject {
     @Published var extraFrosting = false
     @Published var addSprinkles = false
     
-    @Published var name = ""
-    @Published var streetAddress = ""
-    @Published var city = ""
-    @Published var zip = ""
+    @Published var name = "baris karalar"
+    @Published var streetAddress = "hooop str"
+    @Published var city = "Berlin"
+    @Published var zip = "10961"
     
     var hasValidAddress: Bool {
         if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
@@ -50,13 +52,54 @@ class Order: ObservableObject {
         }
         
         // $0.50/cake for sprinkles
-           if addSprinkles {
-              cost += Double(quantity) / 2
+        if addSprinkles {
+            cost += Double(quantity) / 2
         }
         
         
         
         return cost
     }
+    
+    // MARK: - Codable
+    
+    enum CodingKeys: CodingKey {
+        case type, quantity, extraFrosting, addSprinkles, name,
+             streetAddress, city, zip
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+        
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+    }
+    
+    required init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+        
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+        
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+    }
+    
+    init() {}
     
 }
